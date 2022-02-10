2a3a3a6d52a383cf26b5f5d4c2885bf86c25e437
Long Phu <phutl@icd-vn.com>
2022-02-10 11:50:51 +0700

    add bottom tab navigation

--- src/navigation/allScreen.tsx
+++ src/navigation/allScreen.tsx
@@ -1,15 +1,15 @@
 import React from "react";
+import Ionicons from "@expo/vector-icons/Ionicons";
 import { BoardingScreen } from "../screens/BoardingScreen/BoardingScreen";
 import { LoginScreen } from "../screens/LoginScreen/LoginScreen";
 import { HomeScreen } from "../screens/HomeScreen/HomeScreen";
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialTopTabNavigator();
 
 export const allScreens = (Stack: any) => {
   return (
     <>
-      
       <Stack.Screen
         name="BoardingScreen"
         component={BoardingScreen}
@@ -22,7 +22,7 @@ export const allScreens = (Stack: any) => {
         component={LoginScreen}
         options={{
           headerShown: false,
-          gestureEnabled: false
+          gestureEnabled: false,
         }}
       />
       <Stack.Screen
@@ -39,35 +39,88 @@ export const allScreens = (Stack: any) => {
 const allTabScreens = (Tab: any) => {
   return (
     <>
+      {}
       <Tab.Screen
-        name="Home"
+        name="home"
         component={HomeScreen}
+        options={{
+          tabBarIcon: ({ focused, color }) => {
+            let iconName = focused ? "home" : "home-outline";
+            return <Ionicons size={24} name={iconName} color={color} />;
+          },
+        }}
       />
       <Tab.Screen
-        name="Wish List"
+        name="favorite"
         component={HomeScreen}
+        options={{
+          tabBarIcon: ({ focused, color }) => {
+            let iconName = focused ? "bookmark" : "bookmark-outline";
+            return <Ionicons size={24} name={iconName} color={color} />;
+          },
+        }}
       />
-      <Tab.Screen
-        name="Notification"
+      <Tab.Screen 
+        name="notification" 
         component={HomeScreen}
+        options={{
+          tabBarIcon: ( {focused, color} ) => {
+            let iconName = focused ? "notifications" : "notifications-outline";
+            return <Ionicons size={24} name={iconName} color={color} />;
+          }
+        }}
       />
-      <Tab.Screen
-        name="Profile"
+      <Tab.Screen 
+        name="profile" 
         component={HomeScreen}
+        options={{
+          tabBarIcon: ( {focused, color} ) => {
+            let iconName = focused ? "person" : "person-outline";
+            return <Ionicons size={24} name={iconName} color={color} />;
+          }
+        }}
       />
     </>
   );
 };
 
 export const tabNavigation = () => {
-    return(
-        <Tab.Navigator
-          tabBarOptions={{
-            activeTintColor: 'black',
-            inactiveTintColor: 'gray',
-          }}
-        >
-            {allTabScreens(Tab)}
-        </Tab.Navigator>
-    )
-}
+  return (
+    <Tab.Navigator
+      tabBarOptions={{
+        showIcon: true,
+        showLabel: false,
+        indicatorStyle: { backgroundColor: "white" },
+        style: { paddingBottom: 31, paddingHorizontal: 42, height: 75 },
+      }}
+      activeTintColor="black"
+      inactiveColor="gray"
+      tabBarPosition="bottom"
+    >
+      {allTabScreens(Tab)}
+    </Tab.Navigator>
+  );
+};
+
+const bottomTabScreen = {
+  home: {
+    component: HomeScreen,
+    fill: "home",
+    outline: "home-outline",
+  },
+  favorite: {
+    component: HomeScreen,
+    fill: "bookmark",
+    outline: "bookmark-outline",
+  },
+  notification: {
+    component: HomeScreen,
+    fill: "notifications",
+    outline: "notifications-outline",
+  },
+  profile: {
+    component: HomeScreen,
+    fill: "person",
+    outline: "person-outline",
+  },
+};
--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { 
+    SafeAreaView,
     ScrollView,
+    Text,
     View,
 } from "react-native";
 
 export const HomeScreen = () => {
     return(
-        <View>
-            
-        </View>
+        <SafeAreaView style={{flex: 1, backgroundColor: 'white', alignItems: 'center', justifyContent: "center"}}>
+            <Text>Home</Text>
+        </SafeAreaView>
     )
 }
