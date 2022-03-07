f8812e2dc37303333f48595f086ea4156aedcadb
Long Phu <phutl@icd-vn.com>
2022-03-07 16:35:22 +0700

    add FCKeyBoardAvoidingView

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
+export { default as FCKeyBoardAvoidingView } from "./FCKeyBoardAvoidingView";
 export { default as Card } from "./Card";
 export { default as IncrementButton } from "./IncrementButton";
-export { default as Header } from './Header';
-export { default as CartItem } from './CartItem';
+export { default as Header } from "./Header";
+export { default as CartItem } from "./CartItem";
