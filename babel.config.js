e2bbf23fdc291af744f69cede6f9af1ab7208107
Long Phu <phutl@icd-vn.com>
2022-05-31 14:16:22 +0700

    config jest

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,11 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: [
+      "babel-preset-expo",
+      ["@babel/preset-env", { targets: { node: "current" } }],
+      '@babel/preset-typescript',
+    ],
     plugins: [
       "react-native-reanimated/plugin",
       [
@@ -9,28 +13,28 @@ module.exports = function (api) {
         {
           root: ["./"],
           extensions: [
-            '.ios.ts',
-            '.android.ts',
-            '.ts',
-            '.ios.tsx',
-            '.android.tsx',
-            '.tsx',
-            '.jsx',
-            '.js',
-            '.json',
+            ".ios.ts",
+            ".android.ts",
+            ".ts",
+            ".ios.tsx",
+            ".android.tsx",
+            ".tsx",
+            ".jsx",
+            ".js",
+            ".json",
           ],
           alias: {
-            "screens": "./src/screens",
-            "navigation": "./src/navigation",
-            "assets": "./src/assets",
-            "config": "./src/config",
-            "components": "./src/components",
-            "reducers": "./src/reducers",
-            "store": "./src/store",
-            "middlewares": "./src/middlewares",
-            "utils": "./src/utils",
-            "services": "./src/services",
-          }
+            screens: "./src/screens",
+            navigation: "./src/navigation",
+            assets: "./src/assets",
+            config: "./src/config",
+            components: "./src/components",
+            reducers: "./src/reducers",
+            store: "./src/store",
+            middlewares: "./src/middlewares",
+            utils: "./src/utils",
+            services: "./src/services",
+          },
         },
       ],
     ],
